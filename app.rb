293b2de9638440c832833df5cdc05cc8b187120a
konatsup <konatsukomatsu@gmail.com>
2019-03-04 19:11:53 +0900

    Add post data action

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
+require './models'
+require './api'
 
 
 get '/' do
@@ -9,4 +11,14 @@ end
 
 get '/posts/new' do
   erb :new
+end
+
+post '/posts/new' do
+  post = Post.create(
+    title: params[:title],
+    description: params[:description],
+    image_url: "http://res.cloudinary.com/konatsup/image/upload/v1551693785/hy9mow1ijkv8z70sgxgv.jpg",
+    music_url: "https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/AudioPreview118/v4/4c/9b/f4/4c9bf4df-3b01-ffbd-6e9e-cf558ac7a4d9/mzaf_4294571228281925337.plus.aac.p.m4a"
+  )
+  redirect '/'
 end
---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Post.create(
+  title: "タイトル",
+      description: "デスクリプション",
+      image_url: "aaaaaaaa",
+      music_url: "iiiiiiii"
+)

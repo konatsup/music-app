c751e41e4e4e7cca1444866855065d187e6d6412
konatsup <konatsukomatsu@gmail.com>
2019-03-04 19:13:37 +0900

    Create api of get post data

---
+++ api.rb
@@ -0,0 +1,34 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+require './models'
+
+get '/api/post' do
+  post = Post.first
+  response = {
+    data: {
+      title: post.title,
+      description: post.description,
+      image_url: post.image_url,
+      music_url: post.music_url
+    }
+  }
+  json response
+end
+
+get '/api/posts' do
+  posts = Post.all
+  ary = []
+  posts.each do |post|
+    ary.push({
+        title: post.title,
+        description: post.description,
+        image_url: post.image_url,
+        music_url: post.music_url
+    })
+  end
+  response = {
+    posts: ary
+  }
+  json response
+end

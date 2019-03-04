2b77447c426d07534e7f4e2ca81b4dea3ebce1fc
konatsup <konatsukomatsu@gmail.com>
2019-03-04 19:52:26 +0900

    Add uploading image action

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@ Bundler.require
 require 'sinatra/reloader' if development?
 require './models'
 require './api'
+require './image_uploader.rb'
 
+require 'open-uri'
+require 'net/http'
+require 'json'
 
 get '/' do
   erb :index
@@ -17,8 +21,12 @@ post '/posts/new' do
   post = Post.create(
     title: params[:title],
     description: params[:description],
-    image_url: "http://res.cloudinary.com/konatsup/image/upload/v1551693785/hy9mow1ijkv8z70sgxgv.jpg",
+    image_url: "",
     music_url: "https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/AudioPreview118/v4/4c/9b/f4/4c9bf4df-3b01-ffbd-6e9e-cf558ac7a4d9/mzaf_4294571228281925337.plus.aac.p.m4a"
   )
+  if params[:image_file]
+    image_upload(params[:image_file])
+  end
+
   redirect '/'
 end
